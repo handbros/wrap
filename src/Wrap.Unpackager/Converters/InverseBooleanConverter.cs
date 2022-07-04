0ddca08a7dc4f23138b486fa8917b5ee2973b792
June <hexstudio16@gmail.com>
2022-07-04 19:21:24 +0900

    [ASSETS] Add localizations, Add converters

---
+++ src/Wrap.Unpackager/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Wrap.Unpackager.Converters
+{
+    [ValueConversion(typeof(bool), typeof(bool?))]
+    public class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool?)value == null)
+            {
+                return null;
+            }
+            else
+            {
+                return !(bool?)value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool?)value == null)
+            {
+                return null;
+            }
+            else
+            {
+                return !(bool)value;
+            }
+        }
+    }
+}
