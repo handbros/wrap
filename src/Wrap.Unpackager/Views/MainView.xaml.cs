339bb7a1c72331d4cb8c87c4b5ae22cccd002171
June <hexstudio16@gmail.com>
2022-07-04 18:49:37 +0900

    [UI] Add essential windows

--- src/Wrap.Unpackager/Views/MainView.xaml.cs
+++ src/Wrap.Unpackager/Views/MainView.xaml.cs
@@ -10,17 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Wpf.Ui.Controls;
 
-namespace Wrap.Unpackager
+namespace Wrap.Unpackager.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// MainView.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainView : UiWindow
     {
-        public MainWindow()
+        public MainView()
         {
             InitializeComponent();
         }
