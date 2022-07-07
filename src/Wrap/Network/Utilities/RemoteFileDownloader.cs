1455784e71a557eee3e9564093a06d8344a79852
June <hexstudio16@gmail.com>
2022-07-07 12:17:22 +0900

    [LIB] Add remote file downloader

---
+++ src/Wrap/Network/Utilities/RemoteFileDownloader.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wrap.Network.Utilities
+{
+    public delegate void ProgressChangedHandler(long? totalFileSize, long totalBytesDownloaded, double? progressPercentage);
+
+    public class RemoteFileDownloader : HttpClient
+    {
+        private readonly string _downloadUrl;
+        private readonly string _destinationFilePath;
+
+        public event ProgressChangedHandler? ProgressChanged;
+
+        public RemoteFileDownloader(string downloadUrl, string destinationFilePath)
+        {
+            _downloadUrl = downloadUrl;
+            _destinationFilePath = destinationFilePath;
+        }
+
+        public async Task DownloadAsync()
+        {
+            using (var response = await GetAsync(_downloadUrl, HttpCompletionOption.ResponseHeadersRead))
+                await DownloadFileFromHttpResponseMessage(response);
+        }
+
+        public async Task DownloadAsync(CancellationToken cancellationToken)
+        {
+            using (var response = await GetAsync(_downloadUrl, HttpCompletionOption.ResponseHeadersRead, cancellationToken))
+            {
+                await DownloadFileFromHttpResponseMessage(response);
+            }
+        }
+
+        private async Task DownloadFileFromHttpResponseMessage(HttpResponseMessage response)
+        {
+            response.EnsureSuccessStatusCode();
+
+            long? totalBytes = response.Content.Headers.ContentLength;
+
+            using (var contentStream = await response.Content.ReadAsStreamAsync())
+            {
+                await ProcessContentStream(totalBytes, contentStream);
+            }
+        }
+
+        private async Task ProcessContentStream(long? totalDownloadSize, Stream contentStream)
+        {
+            long totalBytesRead = 0L;
+            long readCount = 0L;
+            byte[] buffer = new byte[8192];
+            bool isMoreToRead = true;
+
+            using (FileStream fileStream = new FileStream(_destinationFilePath, FileMode.Create, FileAccess.Write, FileShare.None, 8192, true))
+            {
+                do
+                {
+                    int bytesRead = await contentStream.ReadAsync(buffer, 0, buffer.Length);
+
+                    if (bytesRead == 0)
+                    {
+                        isMoreToRead = false;
+                        continue;
+                    }
+
+                    await fileStream.WriteAsync(buffer, 0, bytesRead);
+
+                    totalBytesRead += bytesRead;
+                    readCount += 1;
+
+                    if (readCount % 10 == 0)
+                    {
+                        TriggerProgressChanged(totalDownloadSize, totalBytesRead);
+                    }
+                }
+                while (isMoreToRead);
+            }
+
+            TriggerProgressChanged(totalDownloadSize, totalBytesRead);
+        }
+
+        private void TriggerProgressChanged(long? totalDownloadSize, long totalBytesRead)
+        {
+            if (ProgressChanged == null)
+                return;
+
+            double? progressPercentage = null;
+            if (totalDownloadSize.HasValue)
+                progressPercentage = Math.Round((double)totalBytesRead / totalDownloadSize.Value * 100, 2);
+
+            ProgressChanged(totalDownloadSize, totalBytesRead, progressPercentage);
+        }
+    }
+}
